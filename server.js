4519dd22786371794f725fd0f1cf1a6bdd7188e0
DemonGodXY <gfxks457@proton.me>
2024-07-11 13:05:02 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,19 +1,25 @@
 #!/usr/bin/env node
 'use strict'
 
-const compression = require('compression')
-const express = require('express')
-const app = express()
-const authenticate = require('./src/authenticate')
-const params = require('./src/params')
-const proxy = require('./src/proxy')
+const compression = require('compression');
+const express = require('express');
+const app = express();
+const authenticate = require('./src/authenticate');
+const params = require('./src/params');
+const proxy = require('./src/proxy');
+const nocache = require('nocache');
 
-const PORT = process.env.PORT || 8080
+const PORT = 8080;
+
+app.set('etag', false);
+app.disable('view cache');
+app.disable('etag');
+app.use(nocache());
 
 app.use(compression({
-  level: 9
+  level: 9;
 }))
-app.enable('trust proxy')
-app.get('/', authenticate, params, proxy)
-app.get('/favicon.ico', (req, res) => res.status(204).end())
-app.listen(PORT, () => console.log(`Worker ${process.pid}: Listening on ${PORT}`))
+app.enable('trust proxy');
+app.get('/', authenticate, params, proxy);
+app.get('/favicon.ico', (req, res) => res.status(204).end());
+app.listen(PORT, () => console.log(`Worker ${process.pid}: Listening on ${PORT}`));
