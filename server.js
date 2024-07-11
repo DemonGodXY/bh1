1562587ac67f87ab156d5fcde366bd03ba6b553e
DemonGodXY <gfxks457@proton.me>
2024-07-11 12:40:20 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,9 @@ const proxy = require('./src/proxy')
 
 const PORT = process.env.PORT || 8080
 
-app.use(compression())
+app.use(compression({
+  level: 9
+}))
 app.enable('trust proxy')
 app.get('/', authenticate, params, proxy)
 app.get('/favicon.ico', (req, res) => res.status(204).end())
